f7096267c47f01c79022e7d5d93af71433d2d5cc
Andre <andreferreira@live.co.uk>
2023-04-10 23:44:22 +0100

    Create and access store. Separate components

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,26 @@
   <main>
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
-      <h1>Pinia Crash Tasks</h1>
+      <h1>{{ taskStore.name }}</h1>
     </header>
+        <div class="task-list">
+          <div 
+            v-for="task, i in taskStore.tasks"
+            :key="`task-${i}`"
+          >
+            <TaskDetails 
+              :task="task"
+            />
+          </div>
+      </div>
   </main>
 </template>
 
 <script setup>
+
+import { useTaskStore } from './stores/TaskStore'
+import TaskDetails from './components/TaskDetails.vue';
+
+const taskStore = useTaskStore()
 </script>
 
